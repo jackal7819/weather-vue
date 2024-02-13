a748c7db02d1088f9729f3c393d2845923ef0c03
Viktor Filippov <jackal3166@gmail.com>
2024-02-14 00:03:15 +0200

    Add nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 	import { RouterLink, RouterView } from 'vue-router';
+	import SiteNavigation from './components/SiteNavigation.vue';
 </script>
 
 <template>
-	<div>
+	<div class='flex flex-col min-h-screen bg-slate-900 text-slate-400'>
+		<SiteNavigation />
 		<RouterView />
 	</div>
 </template>
---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,27 @@
+<script setup>
+	import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+	<header class="sticky top-0 shadow-lg bg-slate-900 shadow-slate-400">
+		<nav
+			class="container flex flex-col items-center gap-4 py-4 mx-auto sm:flex-row w-[90vw]"
+		>
+			<RouterLink :to="{ name: 'home' }">
+				<div class="flex items-center flex-1 gap-3">
+					<i class="text-2xl fa-solid fa-sun"></i>
+					<h1 class="text-2xl">The Local Weather</h1>
+				</div>
+			</RouterLink>
+
+			<div class="flex justify-end flex-1 gap-3">
+				<i
+					class="text-2xl duration-300 cursor-pointer fa-solid fa-circle-info hover:text-sky-400"
+				></i>
+				<i
+					class="text-2xl duration-300 cursor-pointer fa-solid fa-plus hover:text-sky-400"
+				></i>
+			</div>
+		</nav>
+	</header>
+</template>
