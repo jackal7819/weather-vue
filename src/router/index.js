831617e598601e753355ec247c7745e492ad254c
Viktor Filippov <jackal3166@gmail.com>
2024-02-16 00:54:12 +0200

    Add previewCity

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CityViewVue from '../views/CityView.vue';
 import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHistory(),
 	routes: [
 		{
 			path: '/',
 			name: 'home',
 			component: HomeView,
+		},
+		{
+			path: '/weather/:region/:city',
+			name: 'cityView',
+			component: CityViewVue,
 		},
 	],
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 	import { ref } from 'vue';
 	import axios from 'axios';
+	import { useRouter } from 'vue-router';
 
 	const token = import.meta.env.VITE_API_KEY;
 	const url = import.meta.env.VITE_API_URL;
@@ -8,6 +9,22 @@
 	const queryTimeout = ref(null);
 	const mapboxSearchResults = ref(null);
 	const searchError = ref(null);
+	const router = useRouter();
+
+	const previewCity = (searchResult) => {
+		const [city, region] = searchResult.place_name.split(',');
+		router.push({
+			name: 'cityView',
+			params: {
+				region: region.replaceAll(' ', ''),
+				city: city.replaceAll(' ', ''),
+			},
+			query: {
+				lat: searchResult.geometry.coordinates[1],
+				lng: searchResult.geometry.coordinates[0],
+			},
+		});
+	};
 
 	const getSearchResults = () => {
 		clearTimeout(queryTimeout.value);
@@ -37,7 +54,7 @@
 					v-model="searchQuery"
 					@input="getSearchResults"
 					class="w-full p-2 bg-transparent border-b-2 border-slate-400 focus:border-sky-700 focus:outline-none"
-					placeholder="Search for a city..."
+					placeholder="Search for a city or a region..."
 				/>
 				<ul
 					class="absolute w-full px-1 py-2 top-[66px] shadow-md shadow-sky-700 bg-slate-400 text-slate-900 rounded"
@@ -57,6 +74,7 @@
 							class="p-2 duration-300 cursor-pointer hover:text-sky-700"
 							v-for="searchResult in mapboxSearchResults"
 							:key="searchResult.id"
+							@click="() => previewCity(searchResult)"
 						>
 							{{ searchResult.place_name }}
 						</li>
