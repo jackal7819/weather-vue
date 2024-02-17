4fb9d6a228a92c2d4b0ff043f5556b4ff77bf5bd
Viktor Filippov <jackal3166@gmail.com>
2024-02-17 23:29:26 +0200

    Add Route Transitions

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,23 @@
 </script>
 
 <template>
-	<div class='flex flex-col min-h-screen bg-slate-900 text-slate-400'>
+	<div class="flex flex-col min-h-screen bg-slate-900 text-slate-400">
 		<SiteNavigation />
-		<RouterView />
+		<RouterView v-slot="{ Component }">
+			<Transition name="page" mode="out-in">
+				<component :is="Component" />
+			</Transition>
+		</RouterView>
 	</div>
 </template>
+
+<style>
+	.page-enter-active,
+	.page-leave-active {
+		transition: all 0.6s ease;
+	}
+	.page-enter-from,
+	.page-leave-to {
+		opacity: 0;
+	}
+</style>
